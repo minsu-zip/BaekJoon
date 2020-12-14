84eca73335e52588359e2f7824cbbbc410c02d25
minsu-zip <alstn4814@gmail.com>
2020-12-15 01:27:51 +0900

    [ADD] 2839_설탕문제

---
+++ Greedy/2839.js
@@ -0,0 +1,42 @@
+// 브론즈 1 설탕문제
+
+function solution(n) {
+    let count = 0;
+
+    while(n > 0){
+        if(n % 5 === 0){
+            n -= 5;
+            count += 1;
+        }
+        else if(n % 3 === 0){
+            n -= 3;
+            count += 1;
+        }
+        else if(n > 5){
+            n -= 5;
+            count += 1;
+        }
+        else{
+            count = -1;
+            break;
+        }
+    }
+    console.log(count);
+
+}
+
+const { count } = require("console");
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = 0;
+rl.on("line", function (line) {
+    input = parseInt(line);
+    rl.close();
+}).on("close", function () {
+    solution(input);
+    process.exit();
+});
