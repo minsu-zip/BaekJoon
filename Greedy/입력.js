f1ea00487ed06c2aaaccf57da6d701cbf8ffed1a
minsu-zip <alstn4814@gmail.com>
2020-12-29 16:30:29 +0900

    [UPDATE] 입력 예시 수정

--- Greedy/입력.js
+++ Greedy/입력.js
@@ -1,5 +1,5 @@
-function solution(input) {
-   console.log(list);
+function solution() {
+   
 }
 
 const readline = require("readline");
@@ -9,23 +9,14 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-let list = [];
 rl.on("line", function (line) {
    //여러줄 입력
    input.push(line)
-
-   //rl.close()가 없어서 계속입력
-   //로컬에서 입력을 중지할려면 입력을 한 후 'ctrl + D'을 통해 입력 종료
 }).on("close", function () {
 
-    // 이런식으로 적절하게 입력값을 처리해줘야한다.
     let n = parseInt(input[0]);
-
-    //띄어쓰기 기준으로 배열에 넣기
     let list = input[1].split(' ').map((el) => parseInt(el));
 
-    solution(list);
-
-    //프로세스 종료
+    solution();
     process.exit();
 });
